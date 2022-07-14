b54e40d83a3fc96f321bbde14167abe7792c2ad3
Scott Brady <scott@scottbrady91.com>
2022-07-14 15:56:55 +0100

    Fixed copy & paste error in sample data

--- samples/ScottBrady.IdentityModel.Samples.AspNetCore/SampleOptions.cs
+++ samples/ScottBrady.IdentityModel.Samples.AspNetCore/SampleOptions.cs
@@ -35,6 +35,6 @@ namespace ScottBrady.IdentityModel.Samples.AspNetCore
             EdDsa.Create(new EdDsaParameters(ExtendedSecurityAlgorithms.Curves.Ed25519) {X =Convert.FromBase64String("doaS7QILHBdnPULlgs1fX0MWpd1wak14r1yT6ae/b4M=")}));
         
         public readonly EdDsaSecurityKey PasetoV2PrivateKey= new EdDsaSecurityKey(
-            EdDsa.Create(new EdDsaParameters(ExtendedSecurityAlgorithms.Curves.Ed25519) {D =Convert.FromBase64String("doaS7QILHBdnPULlgs1fX0MWpd1wak14r1yT6ae/b4M=")}));
+            EdDsa.Create(new EdDsaParameters(ExtendedSecurityAlgorithms.Curves.Ed25519) {D =Convert.FromBase64String("TYXei5+8Qd2ZqKIlEuJJ3S50WYuocFTrqK+3/gHVH9B2hpLtAgscF2c9QuWCzV9fQxal3XBqTXivXJPpp79vgw==")}));
     }
 }
