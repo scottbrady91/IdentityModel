aa7e98774f985b829c2852608a5090122d3160bb
Thomas Tran <thomas.tran@hostplus.com.au>
2024-03-01 14:26:31 +1100

    added feature to convert to JsonWebKey

---
+++ src/ScottBrady.IdentityModel/Extensions/ExtendedJsonWebKeyConverter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using ScottBrady.IdentityModel.Crypto;
+using ScottBrady.IdentityModel.Tokens;
+
+namespace ScottBrady.IdentityModel.Extensions
+{
+    public static class ExtendedJsonWebKeyConverter
+    {
+        public static JsonWebKey ConvertFromEdDsaSecurityKey(EdDsaSecurityKey securityKey)
+        {
+            var parameters = securityKey.EdDsa.Parameters;
+            return new JsonWebKey
+            {
+                Crv = parameters.Curve,
+                X = parameters.X != null ? Base64UrlEncoder.Encode(parameters.X) : null,
+                D = parameters.D != null ? Base64UrlEncoder.Encode(parameters.D) : null,
+                Kty = ExtendedJsonWebAlgorithmsKeyTypes.ECDH,
+                Alg = ExtendedSecurityAlgorithms.EdDsa,
+                CryptoProviderFactory = securityKey.CryptoProviderFactory,
+            };
+        }
+    }
+}
